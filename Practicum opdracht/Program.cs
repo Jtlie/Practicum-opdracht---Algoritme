57dd1723bc78db2f65c2b82682f47986a733edc9
Jtlie <jtlie@live.nl>
2015-01-15 03:16:30 +0100

    Netjes gemaakt en verwijder functie af (niet getest)

--- Practicum opdracht/Program.cs
+++ Practicum opdracht/Program.cs
@@ -12,8 +12,44 @@ namespace Practicum_opdracht
         {
             Bestelling[] queue = new Bestelling[13];
             queue = Vul_Array(queue);
-           // queue = Bestelling_Toevoegen(queue);
-            Verwijder_Complete_Bestellingen(queue);
+            Console.WriteLine("Kies uw nummer: \n 1 - Bestelling Toevoegen \n 2 - Verwijder Complete Bestellingen");
+            int opdracht = 0;
+            try
+            {
+                opdracht = Int32.Parse(Console.ReadLine());
+            }
+            catch
+            {
+               
+            }
+            switch (opdracht)
+            {
+                case 1:
+                    queue = Bestelling_Toevoegen(queue);
+                    break;
+                case 2:
+                    Console.WriteLine("Weet u Zeker dat U ze wilt verwijderen? [j/n]");
+                    string Antwoord = Console.ReadLine();
+                    if(Antwoord == "j" | Antwoord =="J")
+                    {
+                        Verwijder_Complete_Bestellingen(queue);
+                        Console.WriteLine("Complete Bestellingen Verwijderd");
+                    }
+                    else if (Antwoord == "n" | Antwoord == "N")
+                    {
+                        Console.WriteLine("Complete Bestellingen  Niet Verwijderd");
+                    }
+                    else
+                    {
+
+
+                        Console.WriteLine("Verkeerde Input \n Complete Bestellingen Niet Verwijderd");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Geen Juiste invoer");
+                    break;
+            }
             Console.ReadKey();
         }
 
@@ -53,25 +89,41 @@ namespace Practicum_opdracht
             Console.WriteLine("andere invoer = Nee");
             bestelling.Dadelijk = Console.ReadLine() == "1";
 
-            queue = Maak_Bestelling_Array_Langer(queue);
+            queue = Voeg_index_Toe(queue);
             queue[queue.Length-1] = bestelling;
+            Console.WriteLine("bestelling toegevoegd");
             return queue;
         }
 
         static Bestelling[] Verwijder_Complete_Bestellingen(Bestelling[] queue)
         {
-            Bestelling[] queue_Oud = queue;
+            int j = 0;
+            int Aantal_Compleet = 0;
+            
+            Bestelling[] queue_Zonder_Complete_Bestellingen = new Bestelling[queue.Length];
             for (int i = 0; i < queue.Length; i++ )
             {
-                if(queue[i].Compleet == true)
+                if(queue[i].Compleet =! true)
                 {
-                    
+                    queue_Zonder_Complete_Bestellingen[j] = queue[i];
+                    j++;
+                }
+                else
+                {
+                    Aantal_Compleet++;
                 }
             }
-                return null;
+            Bestelling[] queue_nieuw = new Bestelling[queue.Length];
+            for (int k = 0; k > Aantal_Compleet; k++)
+            {
+                queue_nieuw = Maak_Bestelling_Array_Korter(queue_nieuw);
+            }
+            Array.Copy(queue_Zonder_Complete_Bestellingen, queue_nieuw, queue_nieuw.Length);
+            return queue_nieuw;
+            
         }
 
-
+        
         static Bestelling[] Vul_Array(Bestelling[] queue)
         {
             
@@ -90,7 +142,7 @@ namespace Practicum_opdracht
             return queue;
         }
 
-        static Bestelling[] Maak_Bestelling_Array_Langer(Bestelling[] Array_Oud)
+        static Bestelling[] Voeg_index_Toe(Bestelling[] Array_Oud)
         {
             Bestelling[] Array_Nieuw = new Bestelling[Array_Oud.Length + 1];
             Array.Copy(Array_Oud, Array_Nieuw, Array_Oud.Length);
@@ -100,7 +152,6 @@ namespace Practicum_opdracht
         static Bestelling[] Maak_Bestelling_Array_Korter(Bestelling[] Array_Oud)
         {
             Bestelling[] Array_Nieuw = new Bestelling[Array_Oud.Length - 1];
-            Array.Copy(Array_Oud, Array_Nieuw, Array_Oud.Length);
             return Array_Nieuw;            
         }
     }

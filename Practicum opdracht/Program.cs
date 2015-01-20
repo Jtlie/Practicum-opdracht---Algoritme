560eb8175ff1573a947909f63f66ace1d44769d4
Jtlie <jtlie@live.nl>
2015-01-20 11:14:30 +0100

    Klanten ( werkt nog niet)

--- Practicum opdracht/Program.cs
+++ Practicum opdracht/Program.cs
@@ -11,12 +11,13 @@ namespace Practicum_opdracht
         static void Main(string[] args)
         {
             Bestelling[] queue = new Bestelling[13];
+            Klant[] Klanten = new Klant[10];
             queue = Vul_Array(queue);
+            Klanten = Vul__Klant_Array(Klanten);
             bool start = true;
             while (start == true)
             {
-                Print_queue(queue);
-                Console.WriteLine("Kies uw nummer: \n 1 - Bestelling Toevoegen \n 2 - Verwijder Complete Bestellingen \n 3 - Bestelling updaten \n 4 - Stop");
+                Console.WriteLine("Kies uw nummer: \n 1 - Bestelling Toevoegen \n 2 - Verwijder Complete Bestellingen \n 3 - Bestelling updaten \n 4 - Stop \n 5 - Print de Queue \n 6 - Print Klanten");
                 int opdracht = 0;
                 try
                 {
@@ -55,6 +56,12 @@ namespace Practicum_opdracht
                     case 4:
                         Console.WriteLine("Process gestopt");
                         start = false;
+                        break;
+                    case 5:
+                        Print_queue(queue);
+                        break;
+                    case 6:
+                        Print_Klanten(Klanten);
                         break;
                     default:
                         Console.WriteLine("Geen Juiste invoer");
@@ -191,6 +198,32 @@ namespace Practicum_opdracht
             return queue;
         }
 
+        static Klant[] Vul__Klant_Array(Klant[] klanten)
+        {
+            Random rnd = new Random();
+            for (int i = 1; i < klanten.Length+1; i++)
+            {
+                Klant klant = new Klant();
+                klant.Achternaam = Random_naam(10);
+                klant.Email = Random_naam(15);
+                int random_geslacht = rnd.Next(1, 2); 
+                if(random_geslacht == 1)
+                {
+                    klant.Geslacht = "m";
+                }
+                else{
+                    klant.Geslacht = "v";
+                }
+                klant.Klant_ID = i;
+                klant.Leeftijd = rnd.Next(18, 60);
+                klant.Plaats = Random_naam(20);
+                klant.Voornaam = Random_naam(6);
+                klant.Tussenvoegsel = Random_naam(3);
+                klanten[i - 1] = klant;
+            }
+            return klanten;
+        }
+
         static Bestelling[] Voeg_index_Toe(Bestelling[] Array_Oud)
         {
             Bestelling[] Array_Nieuw = new Bestelling[Array_Oud.Length + 1];
@@ -203,7 +236,20 @@ namespace Practicum_opdracht
             Bestelling[] Array_Nieuw = new Bestelling[Array_Oud.Length - 1];
             return Array_Nieuw;            
         }
+        static string Random_naam(int size)
+        {
+            Random random = new Random((int)DateTime.Now.Ticks);
+            StringBuilder builder = new StringBuilder();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
 
+            return builder.ToString();
+
+        }
         static void Print_queue(Bestelling[] queue)
         {
             for(int i = 0; i < queue.Length; i++)
@@ -216,6 +262,23 @@ namespace Practicum_opdracht
                 Console.WriteLine("Duur:           " + queue[i].Duur);
                 Console.WriteLine("Compleet:       " + queue[i].Compleet);
                 Console.WriteLine("Dadelijk:       " + queue[i].Dadelijk);
+                Console.WriteLine("");
+            }
+        }
+
+        static void Print_Klanten(Klant[] Klanten)
+        {
+            for (int i = 0; i < Klanten.Length; i++)
+            {
+                Console.WriteLine("Klant " + i);
+                Console.WriteLine("Klant_ID:       " + Klanten[i].Klant_ID);
+                Console.WriteLine("Voornaam:       " + Klanten[i].Voornaam);
+                Console.WriteLine("Tussenvoegsel:  " + Klanten[i].Tussenvoegsel);
+                Console.WriteLine("Achternaam:     " + Klanten[i].Achternaam);
+                Console.WriteLine("Leeftijd:       " + Klanten[i].Leeftijd);
+                Console.WriteLine("Geslacht:       " + Klanten[i].Geslacht);
+                Console.WriteLine("Plaats:         " + Klanten[i].Plaats);
+                Console.WriteLine("E-mail:         " + Klanten[i].Email);
                 Console.WriteLine("");
             }
         }
